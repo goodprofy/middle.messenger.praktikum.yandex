5315c896bbf2714c1fc6a3904b5073d63f15aa96
Yuriy Dolganov <jetscript@gmail.com>
2024-06-14 18:01:11 +0500

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Button } from "./components";
+import { Layout } from "./layout";
+import "./styles.scss";
 
 export const App = () => {
   return (
-    <main className="hello">
-      <h1>Hello JSX!</h1>
-      <Button
-        onClick={() => {
-          alert("Hi");
-        }}
-      >
-        Love button
-      </Button>
-    </main>
+    <Layout>
+      <main className="hello">
+        <h1>Hello JSX!</h1>
+        <Button
+          onClick={() => {
+            alert("Hi");
+          }}
+        >
+          Love button
+        </Button>
+      </main>
+    </Layout>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import { App } from "./App";
 
 try {
   const root = document.getElementById("root")!;
-  root?.append(<App />);
+  const appNode = (<App />) as unknown as HTMLElement;
+  root?.append(appNode);
 } catch (err) {
   console.error(err);
 }

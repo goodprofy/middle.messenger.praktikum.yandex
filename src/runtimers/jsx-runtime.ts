6ae0dbd48c93da95891b02f808e28db76cd76e4c
Yuriy Dolganov <jetscript@gmail.com>
2024-06-14 23:47:49 +0500

    exclude undefined attrs from h

--- src/runtimers/jsx-runtime.ts
+++ src/runtimers/jsx-runtime.ts
@@ -1,12 +1,14 @@
+import { isDefined } from '../utils';
+
 export function h(
-  tag: keyof HTMLElementTagNameMap | FunctionComponent<any>,
+  tag: keyof HTMLElementTagNameMap | FC<any>,
   props: { [key: string]: any },
   ...children: any[]
 ): HTMLElement {
-  if (typeof tag === "function") {
+  if (typeof tag === 'function') {
     const result = tag({ ...props, children });
     if (result === null) {
-      throw new Error("Function component returned null.");
+      throw new Error('Function component returned null.');
     }
     return result as unknown as HTMLElement;
   }
@@ -14,27 +16,23 @@ export function h(
   const element = document.createElement(tag);
 
   Object.entries(props || {}).forEach(([key, value]) => {
-    if (
-      key.startsWith("on") &&
-      typeof value === "function" &&
-      key.toLowerCase() in window
-    ) {
+    if (key.startsWith('on') && typeof value === 'function' && key.toLowerCase() in window) {
       element.addEventListener(key.toLowerCase().substring(2), props[key]);
-    } else if (key === "className") {
-      element.classList.add(...((value as string) || "").trim().split(" "));
-    } else {
+    } else if (key === 'className') {
+      element.classList.add(...((value as string) || '').trim().split(' '));
+    } else if (isDefined(value)) {
       element.setAttribute(key, value);
     }
   });
 
   children.forEach((child) => {
-    if (typeof child === "string") {
+    if (typeof child === 'string') {
       element.innerText += child;
     } else if (child instanceof Node) {
       element.appendChild(child);
     } else if (Array.isArray(child)) {
       child.forEach((nestedChild) => {
-        if (typeof nestedChild === "string") {
+        if (typeof nestedChild === 'string') {
           element.innerText += nestedChild;
         } else if (nestedChild instanceof Node) {
           element.appendChild(nestedChild);

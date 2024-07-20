f7b2802a36a35fa476b94fd4a0adc8d2890f6f78
Yuriy Dolganov <jetscript@gmail.com>
2024-07-20 15:52:30 +0500

    add api url

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,5 @@ export const LOGIN_REG_EXP = /(?=.*[a-zA-Z])[a-zA-Z0-9_\-]*/;
 export const NAME_REG_EXP = /[A-ZА-Я][a-zа-я\-]+/;
 export const EMAIL_REG_EXP = /[a-zA-Z0-9_\-]+@[a-zA-Z]+(?:\.[a-zA-Z]+)+/;
 export const PHONE_REG_EXP = /\+?\d{10,15}/;
+
+export const API_BASE_URL = 'https://ya-praktikum.tech/api/v2/';

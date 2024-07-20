5f35adb16bfce77af5d74f53da73d68ffdcf18ce
Yuriy Dolganov <jetscript@gmail.com>
2024-07-20 15:56:38 +0500

    add form generic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,10 @@
-type Values = Record<string, unknown>;
+type Props<T> = { checkValidity?: (state: boolean) => void; onSubmit?: (values: T) => void };
 
-type Props = { checkValidity?: (state: boolean) => void; onSubmit?: (values: Values) => void };
-
-export const Form: FC<PropsWithChildren<Props>> = ({ children, checkValidity, onSubmit }) => {
+export const Form = <T extends Record<string, unknown>>({
+  children,
+  checkValidity,
+  onSubmit
+}: PropsWithChildren<Props<T>>) => {
   const onFormSubmit = (event: Event) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
@@ -16,26 +18,21 @@ export const Form: FC<PropsWithChildren<Props>> = ({ children, checkValidity, on
     }
 
     if (onSubmit) {
-      const data = new FormData(form);
-      let values: Values = {};
-
-      data.forEach((value, field) => {
-        const element = form.elements.namedItem(field) as HTMLInputElement | RadioNodeList;
-
+      const formElements = Array.from(form.elements);
+      const values = formElements.reduce((acc, element) => {
         if (element instanceof HTMLInputElement) {
           if (element.type === 'checkbox' || element.type === 'radio') {
-            values = { ...values, [field]: element.checked };
+            acc = { ...acc, [element.name]: element.checked };
+          } else if (element.type === 'file') {
+            const file = new File([element.value], element.value);
+            acc = { ...acc, [element.name]: file };
           } else {
-            values = { ...values, [field]: value };
+            acc = { ...acc, [element.name]: element.value };
           }
-        } else if (element instanceof RadioNodeList) {
-          element.forEach((radio) => {
-            if ((radio as HTMLInputElement).checked) {
-              values = { ...values, [field]: (radio as HTMLInputElement).value };
-            }
-          });
         }
-      });
+        return acc;
+      }, {} as T);
+
       onSubmit(values);
     }
   };

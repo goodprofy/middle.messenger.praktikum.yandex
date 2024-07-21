74812614f1cd579f8facaf06fb587a07e88b0140
Yuriy Dolganov <jetscript@gmail.com>
2024-07-22 00:00:44 +0500

    add backdrop click on modal avatar

--- src/pages/AvatarEdit/AvatarEdit.tsx
+++ src/pages/AvatarEdit/AvatarEdit.tsx
@@ -1,9 +1,15 @@
 import { Modal } from '../../components';
+import { useRouter } from '../../hooks';
 import { AvatarForm } from './components';
 
 export const AvatarEdit: FC = () => {
+  const { navigate } = useRouter();
+  const onBackdropClick = () => {
+    navigate('/settings');
+  };
+
   return (
-    <Modal title="Загрузите файл">
+    <Modal title="Change avatar" onBackdropClick={onBackdropClick}>
       <AvatarForm />
     </Modal>
   );

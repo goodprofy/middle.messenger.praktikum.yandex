d22608caa153b72250c72a09e7c69a6b5945da84
Yuriy Dolganov <jetscript@gmail.com>
2024-07-21 23:00:39 +0500

    fix isEqual

--- src/utils/isEqual.ts
+++ src/utils/isEqual.ts
@@ -1,40 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type PlainObject<T = any> = {
-  [k in string]: T;
-};
+import type { NonNullableObject } from '../types';
 
-function isPlainObject(value: unknown): value is PlainObject {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    value.constructor === Object &&
-    Object.prototype.toString.call(value) === '[object Object]'
-  );
-}
-
-function isArray(value: unknown): value is [] {
-  return Array.isArray(value);
-}
-
-function isArrayOrObject(value: unknown): value is [] | PlainObject {
-  return isPlainObject(value) || isArray(value);
-}
-
-export function isEqual(lhs: PlainObject, rhs: PlainObject) {
+export function isEqual<T extends object>(lhs: NonNullableObject<T>, rhs: NonNullableObject<T>): boolean {
   if (Object.keys(lhs).length !== Object.keys(rhs).length) {
     return false;
   }
 
-  for (const [key, value] of Object.entries(lhs)) {
+  for (const key in lhs) {
+    const leftValue = lhs[key];
     const rightValue = rhs[key];
-    if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
-      if (isEqual(value, rightValue)) {
-        continue;
+    if (leftValue !== null && rightValue !== null && typeof leftValue === 'object' && typeof rightValue === 'object') {
+      if (!isEqual(leftValue, rightValue)) {
+        return false;
       }
-      return false;
-    }
-
-    if (value !== rightValue) {
+    } else if (leftValue !== rightValue) {
       return false;
     }
   }

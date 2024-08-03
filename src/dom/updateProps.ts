b2a8703feac210072c8902dab16d57eabbbb66e3
Yuriy Dolganov <jetscript@gmail.com>
2024-08-03 15:21:10 +0500

    fix types updateProps

--- src/dom/updateProps.ts
+++ src/dom/updateProps.ts
@@ -1,71 +1,69 @@
-import { VNode } from '../types';
+import type { VNode } from '../types';
 import { isDefined } from '../utils';
 
 export function updateProps(
-  element: HTMLElement | SVGElement | Text,
+  element: Exclude<VNode['element'], undefined>,
   newProps: VNode['props'] = {},
   oldProps: VNode['props'] = {}
 ) {
-  console.group('updateProps');
-  console.log('element', element.outerHTML);
-  console.log({ newProps, oldProps });
   if (!(element instanceof HTMLElement)) {
-    console.groupEnd();
     return;
   }
 
   const allProps = { ...oldProps, ...newProps };
 
   Object.keys(allProps).forEach((name) => {
-    if (name === 'children') {
-      console.groupEnd();
+    if (name === 'children' && newProps[name] !== oldProps[name]) {
       return;
     }
 
-    const oldValue = oldProps[name];
     const newValue = newProps[name];
+    const oldValue = oldProps[name];
+    const isFunction = typeof newValue === 'function';
+    const isObject = typeof newValue === 'object' && newValue !== null;
+    const isString = typeof newValue === 'string';
 
-    if (name === 'ref' && typeof newValue === 'function') {
+    if (name === 'ref' && isFunction) {
       newValue(element);
-    } else if (name.startsWith('on') && typeof newValue === 'function') {
+    } else if (name.startsWith('on')) {
       const eventName = name.toLowerCase().substring(2);
-      if (newValue !== oldValue) {
-        if (isDefined(oldValue)) {
-          element.removeEventListener(eventName, oldValue as EventListener);
-        }
-        if (isDefined(newValue)) {
-          element.addEventListener(eventName, newValue as EventListener);
-        }
+      if (!isDefined(newValue) && isDefined(oldValue)) {
+        element.removeEventListener(eventName, oldValue as EventListener);
+      } else if (isDefined(newValue) && isFunction) {
+        element.addEventListener(eventName, newValue as EventListener);
       }
     } else if (name === 'style') {
-      if (typeof newValue === 'object') {
-        Object.entries(newValue).forEach(([prop, value]) => {
-          if (element.style[prop as any] !== value) {
-            element.style[prop as any] = value;
+      if (isObject) {
+        const styles = Object.entries(newValue);
+        styles.forEach(([prop, value]) => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const styleName = prop as any;
+          if (element.style[styleName] !== value) {
+            element.style[styleName] = value;
           }
         });
-      } else if (typeof newValue === 'string') {
+      } else if (isString) {
         element.setAttribute('style', newValue);
       } else {
+        element.style.cssText = '';
         element.removeAttribute('style');
       }
     } else if (name === 'class') {
-      if (isDefined(newValue)) {
+      if (isDefined(newValue) && isString) {
         element.className = newValue;
       } else {
         element.removeAttribute('class');
       }
-    } else if (newValue !== oldValue) {
+    } else {
       if (name === 'value' && element instanceof HTMLInputElement) {
-        if (element.value !== newValue) {
+        if (element.value !== newValue && isString) {
           element.value = newValue;
         }
-      } else if (newValue != null) {
+      } else if (isString) {
         element.setAttribute(name, newValue);
       } else {
         element.removeAttribute(name);
       }
     }
   });
-  console.groupEnd();
 }
